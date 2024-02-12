25f180f1edb76df47d7f3e0aa9b0233c7d828f88
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-02-12 06:11:03 +0000

    ジャンル作成一覧

--- app/controllers/admin/regions_controller.rb
+++ app/controllers/admin/regions_controller.rb
@@ -1,7 +1,25 @@
 class Admin::RegionsController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def create
+    #データを受け取り新規登録するためのインスタンス作成
+    region = Region.new(region_params)
+    #データをデータベースに保持するためのsaveメソッド実行
+    region.save
+    redirect_to admin_regions_path
+  end
+  
   def index
+    @region = Region.new
+    @regions = Region.all
   end
 
   def edit
+  end
+  
+  private
+  
+  def region_params
+    params.require(:region).permit(:name)
   end
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,2 +1,5 @@
 class Region < ApplicationRecord
+  has_many :groups
+
+  validates :name, presence: true
 end

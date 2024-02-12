7906ad6a25d175ee87a8a85a490e6a1d8643d4c7
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-02-12 19:15:58 +0000

    グループ編集画面

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -20,11 +20,20 @@ class Admin::GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-    # byebug
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
+  
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+       redirect_to admin_group_path(@group)
+    else
+      render :edit
+    end
+   end
   
   private
   

fdd82af52b4e18065e2f10e2fcf4a4996479ea2f
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-02-15 07:07:19 +0000

    投稿詳細画面

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,6 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_member!
-  
+
   def create
     @group = Group.find(params[:group_id])
     #取得したグループに関連づいた新し投稿を作成
@@ -9,25 +9,38 @@ class Public::PostsController < ApplicationController
     @post.member_id = current_member.id
     if @post.save
       flash[:notice] = "投稿しました。"
-      redirect_to action: :index
+      redirect_to group_post_path(group_id: params[:group_id], id: @post)
     else
       render "groups/show"
     end
   end
-  
+
   def index
     @group = Group.find(params[:group_id])
     @posts = @group.posts
   end
 
   def show
+    @post = Post.find(params[:id])
+    @group_id = @post.group_id
   end
 
   def edit
   end
   
-  private
+  def update
+  end
   
+  def destroy
+    @post = Post.find(params[:id])
+    #削除された投稿が所属していたグループのIDを取得
+    @group_id = @post.group_id
+    @post.destroy
+    redirect_to group_posts_path(@group_id)
+  end
+
+  private
+
   def post_params
     params.require(:post).permit(:body)
   end

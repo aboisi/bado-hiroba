91afeef034913cbdc262613c4f2371cd42ed300f
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-02-21 11:41:37 +0000

    レイアウト調整中

--- app/models/member.rb
+++ app/models/member.rb
@@ -60,15 +60,6 @@ class Member < ApplicationRecord
   end
 
   #会員検索
-  # def self.looks(search, word)
-  #   if search == "perfect_match"
-  #     @member = Member.where("full_name LIKE?", "#{word}")
-  #   elsif search == "partial_match"
-  #     @member = Member.where("last_name LIKE ? OR first_name LIKE ? OR full_name LIKE ?", "%#{word}%", "%#{word}%", "%#{word}%")
-  #   else
-  #     @member = Member.none
-  #   end
-  # end
   def self.looks(search, word)
     if search == "perfect_match"
       @members = Member.where("CONCAT(last_name, first_name) LIKE ?", "%#{word}%")

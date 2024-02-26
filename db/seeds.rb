dccba0670bc899bbef126600949ba7a45b95da1e
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-02-26 13:13:45 +0000

    group.address名修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,42 @@ Admin.create!(
    password: '111111'
 )
 
+tanaka.id = Member.find_or_create_by!(email: "tanaka@example.com") do |member|
+  member.last_name = "田中"
+  member.first_name = "太郎"
+  member.display_name = "たなか"
+  member.is_active = true
+  member.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+sato.id = Member.find_or_create_by!(email: "sato@example.com") do |member|
+  member.last_name = "佐藤"
+  member.first_name = "二郎"
+  member.display_name = "さとう"
+  member.is_active = true
+  member.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+end
+
+suzuki.id = Member.find_or_create_by!(email: "suzuki@example.com") do |member|
+  member.last_name = "鈴木"
+  member.first_name = "三郎"
+  member.display_name = "すずき"
+  member.is_active = true
+  member.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+end
+
+kita.id = Region.find_or_create_by!(name: "熊本市北区") 
+
+nishi.id = Region.find_or_create_by!(name: "熊本市西区") 
+
+higashi.id = Region.find_or_create_by!(name: "熊本市東区") 
+
+Group.find_or_create_by!(name: "北区体育館") do |group|
+  group.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-group1.jpg"), filename:"sample-group1.jpg")
+  group.region_id = kita.id
+  group.introduction = "(利用可能時間) 9:00~22:00 (駐車場)有り"
+  group.address = 
+end

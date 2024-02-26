f4faae1138963cd4e5c5d5bdeed0445c07ad8b77
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-02-26 12:10:42 +0000

    sample画像追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,21 +5,4 @@ class Favorite < ApplicationRecord
 
   #1人のユーザーが1つの投稿に対して重複していいねできないバリデーション(member_idとpost_idが一意である)
   validates :member_id, uniqueness: {scope: :post_id}
-
-  def group
-    post.group
-  end
-  
-  def favorited_by?(member)
-    self.member == member
-  end
-
-  # いいねをつけた投稿の取得
-  def self.favorite_posts(member, page, per_page) # モデル内での操作を開始
-    includes(post: [:group, :member]) # postと関連するgroupとmemberも取得する
-      .where(posts: {member_id: member.id }) # ユーザーがいいねしたレコードを絞り込み
-      .order(created_at: :desc) # 投稿を作成日時の降順でソート
-      .page(page) # ページネーションのため、指定ページに表示するデータを選択
-      .per(per_page) # ページごとのデータ数を指定
-  end
 end
